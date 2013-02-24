93a1e8d7da2995391739386ac2a129f66431f56c
progand <lach@ukr.net>
2013-02-24 11:59:02 +0200

    [*] Рефракторинг - убраны ненужные команды

--- mvc/controllers/FrontController.php
+++ mvc/controllers/FrontController.php
@@ -1,19 +1,16 @@
 <?php
 
 header('Content-Type: text/html; charset=cp-1251');
-require_once $_SERVER['DOCUMENT_ROOT'] . '/mvc/commands/IndexCommand.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/mvc/commands/ContactsCommand.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mvc/commands/GalleryCommand.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mvc/commands/ImageCommand.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mvc/commands/SlideShowCommand.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/mvc/commands/ReportsCommand.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mvc/commands/APICommand.php';
 
 class FrontController {
 
     public function dispatch() {
 
-        $command = new IndexCommand();
+        $command = new BaseCommand();
         if (array_key_exists("action", $_REQUEST)) {
 
             switch ($_REQUEST["action"]) {
