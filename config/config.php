065732b9b49582d20a5172fc61857bf17998025a
progand <lach@ukr.net>
2014-10-22 14:31:12 +0300

    [*] scripts moved down

--- config/config.php
+++ config/config.php
@@ -1,10 +1,17 @@
 <?php
 
-
-define('DB_HOST', 'localhost');
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+define('DB_HOST', 's4.ho.ua');
 define('DB_DATABASE', 'zbar');
-define('DB_USER', 'root');
-define('DB_PASS', '123');
+define('DB_USER', 'zbar');
+define('DB_PASS', '1451144994');
 define('HOST_URI', 'http://zbar.ho.ua/');
 
+/*$DB_HOST='localhost';
+$DB_DATABASE='zbar';
+$DB_USER='root';
+$DB_PASS='';*/
 ?>
